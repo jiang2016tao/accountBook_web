cedd3eff19fd2ff5f667171e60a93004dbf72cbd
jiang2016tao <824167117@qq.com>
2019-05-11 22:29:36 +0800

    系统日志不同级别日志，写入不同的文件中

--- src/main/java/com/jiang/controller/UserController.java
+++ src/main/java/com/jiang/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.jiang.controller;
 
+
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,9 +15,15 @@ public class UserController {
 
 	private UserService userService=HessianServiceUtil.getUserService();
 	
+	private Logger logger=Logger.getLogger( UserController.class );
+	
 	@RequestMapping(value="testUser")
 	@ResponseBody
 	public String testUser(){
+		logger.debug( "debug log" );
+		logger.info( "info log" );
+		logger.warn( "warn log" );
+		logger.error( "error log" );
 		return userService.login();
 	}
 }
